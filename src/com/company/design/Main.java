88c184048f347e2a922b477680492e151864f382
naekang <66901361+naekang@users.noreply.github.com>
2021-05-23 16:49:24 +0900

    adapter design pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -7,15 +8,36 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-	// Singleton Pattern Example
+//	// Singleton Pattern Example
+//
+//        AClazz aClazz = new AClazz();
+//        BClazz bClazz = new BClazz();
+//
+//        SocketClient aClient = aClazz.getSocketClient();
+//        SocketClient bClient = bClazz.getSocketClient();
+//
+//        System.out.println("두개의 객체가 동일한가?");
+//        System.out.println(aClient.equals(bClient));
 
-        AClazz aClazz = new AClazz();
-        BClazz bClazz = new BClazz();
+        // Adapter design pattern
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
+        // 220v를 상속받았기 떄문에 문제가 생김
+        Cleaner cleaner = new Cleaner();
+        // 어댑터 사오기
+        Electronic110V adapter = new SocketAdapter(cleaner);
 
-        System.out.println("두개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
+        Airconditioner airconditioner = new Airconditioner();
+        Electronic110V airAdapter = new SocketAdapter(airconditioner);
+
+        connect(airAdapter);
+
+//        connect(cleaner);
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }
---
+++ src/com/company/design/adapter/Electronic110V.java
@@ -0,0 +1,7 @@
+package com.company.design.adapter;
+
+public interface Electronic110V {
+
+    // 전원을 준다
+    void powerOn();
+}
