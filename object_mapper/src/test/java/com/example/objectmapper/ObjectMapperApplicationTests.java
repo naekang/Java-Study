061983ec419e6d82364f853aa2b82223f4b235cf
naekang <66901361+naekang@users.noreply.github.com>
2021-05-28 16:46:14 +0900

    object mapper practice

---
+++ object_mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example.objectmapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMapperApplicationTests {
+
+    @Test
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("---------------");
+
+        // Text JSON -> Object
+        // Object -> Text JSON
+
+        // controller req json(text) -> object
+        // response object -> json(text)
+
+        var objectMapper = new ObjectMapper();
+
+        // object -> text
+        // object mapper get method 활용함
+        var user = new User("steve", 10, "010-3230-2498");
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        // text = object
+        // object mapper는 default 생성자를 필요로 함
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
+
+    }
+
+}
